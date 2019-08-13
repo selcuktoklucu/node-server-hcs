012563dc88d2b7e50330866035d3d889b474a5d8
selcuktoklucu <selcuktoklucu@gmail.com>
2019-08-13 13:37:26 -0400

    User Routes added Express generator used nodemon server implemented npm passport installed npm bcrypt install babel core installed .babel created .env created dotenv installed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+
+// ----USERS
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port${process.env.PORT}!`),
+);
 
 const userCredentials = { firstname: 'Robin' };
 const userDetails = { nationality: 'German' };
